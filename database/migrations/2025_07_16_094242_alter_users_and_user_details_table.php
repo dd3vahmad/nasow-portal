2aaa09537b50d4778215e20456d4513b051a81a3
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-07-16 10:46:42 +0100

    Fix: remove role_id from users table and category from user details table.

---
+++ database/migrations/2025_07_16_094242_alter_users_and_user_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table("users", function (Blueprint $table) {
+            $table->removeColumn("role_id");
+        });
+
+        Schema::table("user_details", function (Blueprint $table) {
+            $table->removeColumn("category");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table("users", function (Blueprint $table) {
+            $table->foreignId("role_id")->references("id")->on("users");
+        });
+
+        Schema::table("user_details", function (Blueprint $table) {
+            $table->enum("category", ["STUD", "ASSOC", "PROF"]);
+        });
+    }
+};
