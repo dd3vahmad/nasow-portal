2698ef166e811c0d764db1b10a29a3a2508cfbbc
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-07-17 15:21:24 +0100

    Fix: Model naming to fit table naming.

--- app/Http/Controllers/UserEducationsController.php
+++ app/Http/Controllers/UserEducationsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserEducations\StoreUserEducationsRequest;
 use App\Http\Responses\ApiResponse;
-use App\Models\UserEducation;
+use App\Models\UserEducations;
 use Illuminate\Support\Facades\Auth;
 
 class UserEducationsController extends Controller
@@ -26,7 +26,7 @@ class UserEducationsController extends Controller
             foreach ($educationsPayload as $educationData) {
                 $educationData['user_id'] = $user->id;
 
-                $education = UserEducation::create($educationData);
+                $education = UserEducations::create($educationData);
 
                 $savedEducations[] = $education;
             }
--- app/Models/UserEducations.php
+++ app/Models/UserEducations.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserEducation extends Model {
+class UserEducations extends Model {
     use HasFactory;
 
     protected $fillable = ['user_id', 'institution', 'course_of_study', 'qualification', 'year_of_graduation'];
