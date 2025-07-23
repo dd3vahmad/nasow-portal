8d42fbcaade749715ea9130e1c92f3fb889e2e02
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-07-23 10:53:49 +0100

    Fix: cpd log validation.

--- app/Http/Requests/CPD/LogCpdActivityRequest.php
+++ app/Http/Requests/CPD/LogCpdActivityRequest.php
@@ -15,6 +15,29 @@ class LogCpdActivityRequest extends BaseRequest
     }
 
     /**
+     * Validation notifiers
+     * @param mixed $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $creditHours = $this->input('credit_hours');
+            $activityId = $this->input('activity_id');
+
+            if (!empty($creditHours) && !empty($activityId)) {
+                $validator->errors()->add('credit_hours', 'You cannot provide both credit hours and an activity.');
+                $validator->errors()->add('activity_id', 'You cannot provide both an activity and credit hours.');
+            }
+
+            if (empty($creditHours) && empty($activityId)) {
+                $validator->errors()->add('credit_hours', 'Either credit hours or activity must be provided.');
+                $validator->errors()->add('activity_id', 'Either credit hours or activity must be provided.');
+            }
+        });
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -30,14 +53,12 @@ class LogCpdActivityRequest extends BaseRequest
                 'between:1,999.9',
                 'regex:/^\d{1,3}(\.\d)?$/',
                 'required_without:activity_id',
-                'prohibited_with:activity_id',
             ],
             'completed_at' => 'required|date|before_or_equal:today',
             'activity_id' => [
                 'nullable',
                 'exists:cpd_activities,id',
                 'required_without:credit_hours',
-                'prohibited_with:credit_hours',
             ],
             'certificate' => 'nullable|file|max:2048',
         ];
