a1736e6ff43265dc9f36591a72d37a416683d094
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-07-26 21:13:37 +0100

    Fix: user employments date columns.

---
+++ database/migrations/2025_07_26_193607_alter_employments_table_to_include_employment_end_date.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_employments', function (Blueprint $table) {
+            $table->dropColumn('year');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_employments', function (Blueprint $table) {
+            $table->date('year');
+            $table->dropColumn('end_date');
+            $table->dropColumn('start_date');
+        });
+    }
+};
