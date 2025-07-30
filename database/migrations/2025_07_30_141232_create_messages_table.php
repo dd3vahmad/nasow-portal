2a16ee13de959d065665093fdc2f5abf400c772f
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-07-30 15:18:16 +0100

    Add: chatting table structure tables.

---
+++ database/migrations/2025_07_30_141232_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chat_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('reply_to')->nullable()->constrained('messages')->onDelete('set null');
+            $table->text('content');
+            $table->enum('type', ['text', 'file', 'image'])->default('text');
+            $table->json('attachments')->nullable();
+            $table->boolean('is_edited')->default(false);
+            $table->timestamp('sent_at')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
