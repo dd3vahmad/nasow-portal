1d2fb922301f7314ac6157fe1ba56b6b88549fa5
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-08-04 15:19:15 +0100

    Add: settings table migration.

---
+++ database/migrations/2025_08_03_221136_create_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('2fa')->default(false);
+            $table->boolean('email_notification')->default(true);
+            $table->boolean('sms_notification')->default(false);
+            $table->enum('color_mode', ['dark', 'light', 'system'])->default('system');
+            $table->string('language')->default('english');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->jsonb('metadata')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
