5cf8bb125ac7529108deca9aa1e441a8441b14db
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-09-05 22:57:18 +0100

    Fix: memebrhsip catgeory objectstructure.

--- app/Http/Controllers/MembershipCategoryController.php
+++ app/Http/Controllers/MembershipCategoryController.php
@@ -10,7 +10,16 @@ class MembershipCategoryController extends Controller
 {
     public function index(Request $request) {
         try {
-            $categories = MembershipCategory::all();
+            $categories = MembershipCategory::all()->map(function ($category) {
+                return [
+                    'id' => $category->id,
+                    'name' => $category->name,
+                    'slug' => $category->slug,
+                    'currency' => $category->currency,
+                    'price' => $category->price,
+                    'updated_at' => $category->updated_at->format('D, jS M, Y'),
+                ];
+            });
 
             return ApiResponse::success("Membership categories fetched successfully.", $categories);
         } catch (\Throwable $th) {
